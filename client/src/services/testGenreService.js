829e593ac7c95357c84e53da387d74bce0f8d6c4
riegit <37225235+riegit@users.noreply.github.com>
2018-10-12 17:51:21 -0400

    Update testGenreService.js

--- client/src/services/testGenreService.js
+++ client/src/services/testGenreService.js
@@ -2,6 +2,7 @@ export const genres = [
   { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
   { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
   { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" }
+  { _id: "5b21ca3eeb7f6fbccd471816", name: "Romance" }
 ];
 
 export function getGenres() {
